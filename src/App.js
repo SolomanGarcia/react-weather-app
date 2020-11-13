7072b336146f5c2aa16f1280147d7f5913f09a42
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-13 09:31:48 -0700

    work on layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import sampleData from './data/sample.json'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+    <Container>
+      <Row>
+        <Col md={7}>
+          <h1>Weather for [city, state]</h1>
+        </Col>
+        <Col md={5}>
+          <h2>Search bar goes here</h2>
+        </Col>
+      </Row>
+      <Row></Row>
+    </Container>
+
 
 export default App;
