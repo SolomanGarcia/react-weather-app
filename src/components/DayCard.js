b25c8ffd7bcd7b3477f5310ea958bdaa7a71f6fb
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-16 07:49:10 -0700

    create DayCard component

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -6,8 +6,20 @@ const DayCard = ({ day, temp, icon, description, high, low, precip }) => {
     <Card>
       <CardHeader>{day}</CardHeader>
       <CardBody>
-        <h2>{temp}</h2>
-        <img src={`${process.env.PUBLIC_URL}/icons/${icon}.png`} />
+        <h2>{temp.toFixed(1)}°</h2>
+        <img
+          src={`${process.env.PUBLIC_URL}/icons/${icon}.png`}
+          alt={description}
+        />
+        <p>
+          <strong>High:</strong> {high.toFixed(1)}°
+        </p>
+        <p>
+          <strong>Low:</strong> {low.toFixed(1)}°
+        </p>
+        <p>
+          <strong>Precip:</strong> {precip}%
+        </p>
       </CardBody>
     </Card>
   );
