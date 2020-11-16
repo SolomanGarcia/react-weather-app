2bb7e53c577641c0177036d6f8c14305c5f5476a
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-16 07:26:53 -0700

    add moment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "reactstrap";
+import moment from "moment";
 import sampleData from "./data/sample.json";
 import SearchBar from "./components/SearchBar";
 import DayCard from "./components/DayCard";
@@ -15,11 +16,13 @@ function App() {
     searchTerm: ""
   });
 
+  const { location, days, selectedDay, searchTerm } = weatherInfo;
+
   return (
     <Container>
       <Row>
         <Col md={7}>
-          <h1>Weather for [city, state]</h1>
+          <h1>Weather for {location}</h1>
         </Col>
         <Col md={5}>
           <h2>
@@ -28,9 +31,17 @@ function App() {
         </Col>
       </Row>
       <Row>
-        {sampleData.data.map((day) => (
-          <Col>
-            <DayCard />
+        {days.map((day) => (
+          <Col key={day.valid_date}>
+            <DayCard
+              icon={day.weather.icon}
+              description={day.weather.description}
+              high={day.high_temp}
+              low={day.low_temp}
+              temp={day.temp}
+              precip={day.pop}
+              day={moment(day.valid_date, "YYYY-MM-DD").format("dddd")}
+            />
           </Col>
         ))}
       </Row>
