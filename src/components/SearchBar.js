369e381571cb83aa8b36d47fafa82f19b41bc276
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-16 16:52:11 -0700

    work on SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { FormGroup, Label, Input, Button } from "reactstrap";
+import { FormGroup, Label, Input, Button, Form } from "reactstrap";
 
 const SearchBar = ({ searchTerm, handleInputChange, handleFormSubmit }) => {
-  return <h2>Im the SearchBar</h2>;
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="searchTerm" hidden>
+          Search by Location:
+        </Label>
+        <Input
+          type="text"
+          name="searchTerm"
+          id="searchTerm"
+          placeholder="Mount Rushmore OR 10014 OR Denver, CO"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+      </FormGroup>
+      <Button>Search</Button>
+    </Form>
+  );
 };
 
 export default SearchBar;
