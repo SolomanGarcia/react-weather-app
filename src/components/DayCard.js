675dd7f04c38d220a19a5d879e3c55936801e18c
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-16 07:41:47 -0700

    hook up icons

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,7 +1,16 @@
 import react from "react";
+import { Card, CardHeader, CardBody } from "reactstrap";
 
-const DayCard = () => {
-  return <span>Daycard</span>;
+const DayCard = ({ day, temp, icon, description, high, low, precip }) => {
+  return (
+    <Card>
+      <CardHeader>{day}</CardHeader>
+      <CardBody>
+        <h2>{temp}</h2>
+        <img src={`${process.env.PUBLIC_URL}/icons/${icon}.png`} />
+      </CardBody>
+    </Card>
+  );
 };
 
 export default DayCard;
